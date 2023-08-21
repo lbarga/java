235128500381018e117daf9505f3b88626189c38
Lucas Barga <lucasmpbarga@gmail.com>
2023-08-21 07:06:40 -0300

    init ms credit cards

---
+++ domine-microservicos-mensageria-com-spring-cloud-docker/mscreditcards/src/main/java/io/github/springcloud/mscreditcards/MscreditcardsApplication.java
@@ -0,0 +1,13 @@
+package io.github.springcloud.mscreditcards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MscreditcardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MscreditcardsApplication.class, args);
+	}
+
+}
