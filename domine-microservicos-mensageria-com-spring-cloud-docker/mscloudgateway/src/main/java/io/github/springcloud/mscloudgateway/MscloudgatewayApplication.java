c439dc666f994ec3432a9432cbe6037ce32e95db
Lucas Barga <lucasmpbarga@gmail.com>
2023-08-21 06:10:51 -0300

    finish mscloudgateway

---
+++ domine-microservicos-mensageria-com-spring-cloud-docker/mscloudgateway/src/main/java/io/github/springcloud/mscloudgateway/MscloudgatewayApplication.java
@@ -0,0 +1,30 @@
+package io.github.springcloud.mscloudgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.Route;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
+public class MscloudgatewayApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(MscloudgatewayApplication.class, args);
+
+	}
+
+	public RouteLocator routes(RouteLocatorBuilder builder) {
+		return builder
+				.routes()
+					.route("mscustomers", r -> r.path("/customers/**")
+							.uri("lb://mscustomers"))
+				.build();
+	}
+
+}
