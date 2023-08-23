376ce99960fb3143c9cd9d738009e622e7b79ee5
Lucas Barga <lucasmpbarga@gmail.com>
2023-08-23 06:44:20 -0300

    init ms credit evaluator

---
+++ domine-microservicos-mensageria-com-spring-cloud-docker/mscreditevaluator/src/main/java/io/github/springcloud/mscreditevaluator/MscreditevaluatorApplication.java
@@ -0,0 +1,13 @@
+package io.github.springcloud.mscreditevaluator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MscreditevaluatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MscreditevaluatorApplication.class, args);
+	}
+
+}
