885f7fb82323b37a0a1a406252e9de8dd2b79376
Lucas Barga <lucasmpbarga@gmail.com>
2023-08-27 21:21:07 -0300

    add some exceptions

--- domine-microservicos-mensageria-com-spring-cloud-docker/mscreditevaluator/src/main/java/io/github/springcloud/mscreditevaluator/application/CreditEvaluatorController.java
+++ domine-microservicos-mensageria-com-spring-cloud-docker/mscreditevaluator/src/main/java/io/github/springcloud/mscreditevaluator/application/CreditEvaluatorController.java
@@ -1,5 +1,7 @@
 package io.github.springcloud.mscreditevaluator.application;
 
+import io.github.springcloud.mscreditevaluator.application.exceptions.CustomerDataNotFoundException;
+import io.github.springcloud.mscreditevaluator.application.exceptions.ErrorCommunicationMicroservicesException;
 import io.github.springcloud.mscreditevaluator.application.service.CreditEvaluatorService;
 import io.github.springcloud.mscreditevaluator.domain.CustomerStatus;
 import lombok.NoArgsConstructor;
@@ -22,9 +24,17 @@ public class CreditEvaluatorController {
     }
 
     @GetMapping(value = "/evaluate", params = "cpf")
-    public ResponseEntity<CustomerStatus> evaluateCustomerStatus(@RequestParam String cpf) {
-        CustomerStatus customerStatus = creditEvaluatorService.getCustomerStatus(cpf);
+    public ResponseEntity evaluateCustomerStatus(@RequestParam String cpf)
+            throws CustomerDataNotFoundException, ErrorCommunicationMicroservicesException
+    {
+        try{
+            CustomerStatus customerStatus = creditEvaluatorService.getCustomerStatus(cpf);
 
-        return ResponseEntity.ok(customerStatus);
+            return ResponseEntity.ok(customerStatus);
+        }catch (CustomerDataNotFoundException e){
+            return ResponseEntity.notFound().build();
+        } catch (ErrorCommunicationMicroservicesException e) {
+            return ResponseEntity.status(e.getStatusCode()).body(e.getMessage());
+        }
     }
 }
